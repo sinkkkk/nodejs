aad94bfaa756505042c546d7fa6c4dab30f573ff
sink <59916608+sinkkkk@users.noreply.github.com>
2021-09-14 09:54:53 +0300

    Add files via upload

---
+++ testjs.js
@@ -0,0 +1,14 @@
+const test1 = 123;
+const test2 = 123;
+const test3 = 123;
+test4 = 123;
+console.log(test1,test2,test3,test4);
+
+function hello() {
+    return "tere planeet";
+}
+
+
+
+
+console.log(hello());
